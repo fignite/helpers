c255fdc8f3f6ac9ba9232f2cc861f84228d463c3
Luke Finch <lukechristianfinch@gmail.com>
2021-04-10 21:51:57 +0100

    added: resize, fixed: parent type of ungroupnode

---
+++ src/helpers/resize.ts
@@ -0,0 +1,30 @@
+/**
+ * Resizes a node, to allow nodes of size < 0.01
+ * A value of zero will be replaced with 1/Number.MAX_SAFE_INTEGER
+ * @param {SceneNode & LayoutMixin} node Node to resize
+ * @param {number} width 
+ * @param {number} height 
+ * @returns Resized Node
+ */
+export function resize(node: SceneNode & LayoutMixin, width: number, height: number){
+    //Workaround to resize a node, if its size is less than 0.01
+
+    //If 0, make it almost zero
+    width === 0 ? width = 1/Number.MAX_SAFE_INTEGER : null;
+    height === 0 ? height = 1/Number.MAX_SAFE_INTEGER : null;
+    let nodeParent = node.parent
+    node.resize(width < 0.01 ? 1 : width,height < 0.01 ? 1 : height)
+    if(width < 0.01 || height < 0.01){
+    let dummy = figma.createRectangle()
+    dummy.resize(width < 0.01 ? 1/width : width,height < 0.01 ? 1/height : height)
+    let group: GroupNode = figma.group([node,dummy],figma.currentPage)
+    group.resize(width < 0.01 ? 1 : width,height < 0.01 ? 1 : height)
+    nodeParent.appendChild(node)
+    group.remove()
+    }
+    return node
+    }
+
+    export function almostZero(){
+        return 1/Number.MAX_SAFE_INTEGER
+    }

66f0f169d9d3a52c0e0de2dda5a21dd06f910674
limitlessloop <5551+limitlessloop@users.noreply.github.com>
2021-05-09 15:43:15 +0100

    fixed exports from repo

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+import { getClientStorageAsync, setClientStorageAsync, updateClientStorageAsync } from './helpers/clientStorage'
+import { dispatchEvent, handleEvent } from './helpers/codeMessageHandler'
 import { copyPaste } from './helpers/copyPaste'
+import { getPluginData, setPluginData, updatePluginData } from './helpers/pluginData'
+import { resize } from './helpers/resize'
+import { ungroupNode } from './helpers/ungroupNode'
 
-export { copyPaste }
+export { getClientStorageAsync, setClientStorageAsync, updateClientStorageAsync, dispatchEvent, handleEvent, copyPaste, getPluginData, setPluginData, updatePluginData, resize, ungroupNode }

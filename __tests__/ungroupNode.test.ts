c5e1754211be54ddf106febd310532a8ccf73c2d
limitlessloop <5551+limitlessloop@users.noreply.github.com>
2021-05-09 17:46:43 +0100

    renamed from ungroupNode to group function

--- __tests__/ungroupNode.test.ts
+++ __tests__/ungroupNode.test.ts
@@ -1,4 +1,4 @@
-import { ungroupNode } from '../src/helpers/ungroupNode';
+import { ungroup } from '../src/helpers/ungroup';
 
 var nodes = [
     figma.createRectangle(),
@@ -10,7 +10,7 @@ var parent = figma.currentPage;
 var nodeGroup = figma.group(nodes, parent)
 
 test('same array of nodes exists on parent', () => {
-    ungroupNode(nodeGroup, parent)
+    ungroup(nodeGroup, parent)
 
     for (let i = 0; i < parent.children.length; i++) {
         expect(parent.children[i]).toBe(nodes[i])

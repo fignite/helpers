6846b2adfd7d33d1b692c9f11cb7948ca4db14f5
Gavin McFarland <5551+limitlessloop@users.noreply.github.com>
2021-09-22 16:36:09 +0100

    messing around with bundling imports and typescript

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+// import babel from 'rollup-plugin-babel';
+// import babelrc from 'babelrc-rollup';
+// import dts from 'rollup-plugin-dts' // Bundles type declarations
+// import typescript from 'rollup-plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
+// import ts from 'rollup-plugin-ts';
+
+// rollup-plugin-typescript doesn't output type declarations so using rollup-plugin-typescript2 for now
+
+
+export default {
+    input: 'src/index.ts',
+    output: {
+        format: 'cjs',
+        file: 'dist/index.js'
+    },
+    plugins: [
+        typescript()
+        // ts()
+        // Don't think this is needed
+        // babel(babelrc())
+    ]
+};

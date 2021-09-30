6229afc8b3b414f84fb976f17461b328ebc8ffda
Gavin McFarland <5551+limitlessloop@users.noreply.github.com>
2021-09-30 14:54:19 +0100

    work in pogress

--- src/helpers/getInstanceCounterpartUsingLocation.ts
+++ src/helpers/getInstanceCounterpartUsingLocation.ts
@@ -1,6 +1,7 @@
 import { getParentInstance } from './getParentInstance'
 import { getNodeLocation } from './getNodeLocation'
 import { getNodeIndex } from './getNodeIndex'
+import { getTopInstance } from './getTopInstance'
 
 /**
  * Provides the counterpart component node to the selected instance node. Rather than use the instance node id, it stores the location of the node and then looks for the same node in the main component.
@@ -9,51 +10,53 @@ import { getNodeIndex } from './getNodeIndex'
  */
 
 // TODO: Should there be two functions?, one that gets original component, and one that gets prototype 
+// Using getTopInstance may return counterpart which has been swapped by the user
 
 export function getInstanceCounterpartUsingLocation(node, parentInstance = getParentInstance(node), location = getNodeLocation(node, parentInstance), parentComponentNode = parentInstance?.mainComponent) {
 
     if (location) {
         location.shift()
 
-        function loopChildren(node, d = 1) {
-            var nodeIndex = location[d]
+        // console.log(location)
 
-            if (node.children) {
-                for (var i = 0; i < node.children.length; i++) {
-                    var child = node.children[i]
+        
 
-                    if (getNodeIndex(child) === nodeIndex) {
+        function loopChildren(children, d = 0) {
+            for (var i = 0; i < children.length; i++) {
+                var child = children[i]
+                var nodeIndex = location[d]
+                // console.log({ current: getNodeIndex(child), desired: nodeIndex }, child.name)
 
-                        if (location.length - 1 === d) {
-                            return child
-                        }
-                        else {
-                            return loopChildren(child, d + 1)
-                        }
+                if (getNodeIndex(child) === nodeIndex) {
+                    // console.log(">>>  ", child.name)
+                    
+                    // console.log(location.length - 1, d)
+                    // If last in array
+                    if (location.length - 1 === d) {
+                        // console.log({ current: getNodeIndex(child), desired: nodeIndex })
+                        return child
                     }
+                    else {
+                        if (child.children) {
+                            // console.log({ current: getNodeIndex(child), desired: nodeIndex })
+                            return loopChildren(child.children, d + 1)
+                        }
+                        // else {
+                        //     console.log({ current: getNodeIndex(child), desired: nodeIndex })
+                        //     console.log(child)
+                        //     // return child
+                        // }
+                    }
+
                 }
 
             }
-            else {
-                return node
-            }
+
+            
         }
 
         if (parentComponentNode && parentComponentNode.children) {
-            for (var i = 0; i < parentComponentNode.children.length; i++) {
-                var componentNode = parentComponentNode.children[i]
-
-                var nodeIndex = location[0]
-                if (getNodeIndex(componentNode) === nodeIndex) {
-                    if (location.length - 1 === 0) {
-                        return componentNode
-                    }
-                    else {
-                        return loopChildren(componentNode)
-                    }
-
-                }
-            }
+            return loopChildren(parentComponentNode.children)
         }
         else {
             return node.mainComponent

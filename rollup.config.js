f75022adf08523b004ff9cd6541876ba995fbc6f
limitlessloop <5551+limitlessloop@users.noreply.github.com>
2021-12-22 20:23:15 +0000

    commented out unused imports

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
-import babel from 'rollup-plugin-babel';
-import babelrc from 'babelrc-rollup';
+// import babel from 'rollup-plugin-babel';
+// import babelrc from 'babelrc-rollup';
 // import dts from 'rollup-plugin-dts' // Bundles type declarations
 // import typescript from 'rollup-plugin-typescript';
 import typescript from 'rollup-plugin-typescript2';

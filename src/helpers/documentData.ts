a7ae43768a65e836584706f67a1709ee1d65a7de
limitlessloop <5551+limitlessloop@users.noreply.github.com>
2022-02-24 23:40:07 +0000

    addeded new helper

---
+++ src/helpers/documentData.ts
@@ -0,0 +1,26 @@
+import { setPluginData, getPluginData, updatePluginData } from './pluginData'
+
+/**
+ * An alias for `figma.root` plugin data
+ * @param {String} key A key to store data under
+ * @param {any} data Data to be stored
+ */
+export function setDocumentData(key: string, data: any) {
+    return setPluginData(figma.root, key, data)
+}
+
+/**
+ * An alias for `figma.root` plugin data
+ * @param {String} key A key to store data under
+ */
+export function getDocumentData(key: string) {
+   return  getPluginData(figma.root, key)
+}
+
+/**
+ * An alias for `figma.root` plugin data
+ * @param {String} key A key to store data under
+ */
+export function updateDocumentData(node: BaseNode, key: string, callback: Function) {
+    return updatePluginData(node, key, callback)
+}

7c3b56b81d7de29790c020f5f62aa22c06530338
limitlessloop <5551+limitlessloop@users.noreply.github.com>
2022-05-06 02:07:34 +0100

    Added comment

--- src/helpers/removeChildren.ts
+++ src/helpers/removeChildren.ts
@@ -5,6 +5,8 @@
 
 export function removeChildren(node: SceneNode & ChildrenMixin) {
   let length = node.children.length;
+
+  // Has to happen in reverse because of order layers are listed
   for (let i = length - 1; i >= 0; i--) {
     node.children[i].remove();
   }

0260f3def36e07c76a72a0bc41559e8a6885cb35
Gavin McFarland <5551+gavinmcfarland@users.noreply.github.com>
2023-04-19 14:41:42 +0100

    Updated typings and copying rotation and name for convertToFrame

--- src/helpers/convertToFrame.ts
+++ src/helpers/convertToFrame.ts
@@ -1,5 +1,33 @@
 import { copyPaste } from "./copyPaste";
 
+function groupToFrame(group) {
+  let groupRotation = group.rotation;
+
+  // Create frame to replace group
+  let frame = figma.createFrame();
+  frame.fills = [];
+  frame.x = group.x;
+  frame.y = group.y;
+  frame.name = group.name;
+
+  frame.resize(group.width, group.height);
+  // Add group to frame so that we can reset their x, y cordinates
+  frame.appendChild(group);
+
+  // Reset x, y, and rotation coordinates
+  group.x = 0;
+  group.y = 0;
+  group.rotation = 0;
+
+  // Ungrouping will automatically make contents children of the frame
+  figma.ungroup(group);
+
+  // Re-apply rotation
+  frame.rotation = groupRotation;
+
+  return frame;
+}
+
 /**
  * Converts an instance, component, or rectangle to a frame
  * @param {SceneNode} node The node you want to convert to a frame
@@ -7,25 +35,37 @@ import { copyPaste } from "./copyPaste";
  */
 
 export function convertToFrame(node) {
+  // Save index, parent and rotation of group before removed
+  let nodeIndex = node.parent.children.indexOf(node);
+  let nodeParent = node.parent;
+  let newFrame;
+
   if (node.type === "INSTANCE") {
-    return node.detachInstance();
+    newFrame = node.detachInstance();
   }
 
   if (node.type === "COMPONENT") {
-    let parent = node.parent;
-
     // This method preserves plugin data and relaunch data
     let frame = node.createInstance().detachInstance();
-    parent.appendChild(frame);
+    // Beacuse `createInstance` doesn't inherit rotation, apparently
+    frame.rotation = node.rotation;
+
+    nodeParent.appendChild(frame);
+
     copyPaste(node, frame, { include: ["x", "y"] });
 
     // Treat like native method
     figma.currentPage.appendChild(frame);
     node.remove();
-    return frame;
+    newFrame = frame;
   }
 
-  if (node.type === "RECTANGLE" || node.type === "GROUP") {
+  if (node.type === "GROUP") {
+    let frame = groupToFrame(node);
+    newFrame = frame;
+  }
+
+  if (node.type === "RECTANGLE") {
     let frame = figma.createFrame();
 
     // FIXME: Add this into copyPaste helper
@@ -33,11 +73,16 @@ export function convertToFrame(node) {
     copyPaste(node, frame);
 
     node.remove();
-    return frame;
+    newFrame = frame;
   }
 
   if (node.type === "FRAME") {
     // Don't do anything to it if it's a frame
-    return node;
+    newFrame = node;
   }
+
+  // Re-insert frame into its original position
+  nodeParent.insertChild(nodeIndex, newFrame);
+
+  return newFrame;
 }
